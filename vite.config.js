3d0c1895fc72f64c5bd98e215f73cea16fdecd8b
snikr11 <atomicjr@rocketmail.com>
2025-08-12 16:58:32 -0400

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '',            // important for root domain
+  base: '',                // correct for a root-domain Pages site
   plugins: [react()],
   build: { outDir: 'dist' }
 })
