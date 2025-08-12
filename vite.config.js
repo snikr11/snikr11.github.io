061136201c048f60bc682abbc234427afe6a4e71
snikr11 <atomicjr@rocketmail.com>
2025-08-12 17:05:57 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '',                // correct for a root-domain Pages site
   plugins: [react()],
-  build: { outDir: 'dist' }
+  server: { port: 5173 },
+  base: '/snikr11.github.io/', // <--- Add this line!
 })
