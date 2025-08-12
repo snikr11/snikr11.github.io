b757bf83ad8b3ecf57374db9805ec7a189419f85
snikr11 <atomicjr@rocketmail.com>
2025-08-12 19:52:04 -0400

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-  base: '/snikr11.github.io/', // <--- Add this line!
+  base: '/', // important for username.github.io sites
 })
