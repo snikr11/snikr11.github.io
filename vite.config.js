e9f6b1d9fad60af97eb5e203f1b7314c1963ce8e
snikr11 <atomicjr@rocketmail.com>
2025-08-12 20:35:23 -0400

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // correct for username.github.io sites
-})
+export default defineConfig({ plugins: [react()], base: '/' })
